77069423580bafe833220dadfbce5b6e35f8e35c
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-06-10 15:19:49 +0200

    mock employees request and assert performance text and link

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,18 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('HOME PAGE', () => {
+  it('visit home page', () => {
     cy.visit('http://localhost:8080/');
+  });
+
+  // it('assert employees data is apearing', () => {
+  //   cy.get('[data-testid="employees-list"]').should('be.visible');
+  // });
+
+  it('asssert performance link', () => {
+    cy.contains('Team performance chart page').click()
+    cy.url().should('include', '/team-performance')
+  });
+
+  it('mock the employees request', () => {
+    cy.request('https://fe-task.getsandbox.com/employees');
   });
 });
