48db32eea2c748123c1184b57d57d8b727b8e3d9
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-06-10 15:41:14 +0200

    assert employees data is apearing

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,16 +3,17 @@ describe('HOME PAGE', () => {
     cy.visit('http://localhost:8080/');
   });
 
-  // it('assert employees data is apearing', () => {
-  //   cy.get('[data-testid="employees-list"]').should('be.visible');
-  // });
+  it('mock the employees request', () => {
+    cy.request('employees');
+  });
+
+  it('assert employees data is apearing', () => {
+    cy.get('.c-users-list__body').should('be.visible');
+  });
 
   it('asssert performance link', () => {
     cy.contains('Team performance chart page').click()
     cy.url().should('include', '/team-performance')
   });
 
-  it('mock the employees request', () => {
-    cy.request('https://fe-task.getsandbox.com/employees');
-  });
 });
