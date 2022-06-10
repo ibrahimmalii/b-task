dc0732c7b418dc565f5213be91a40747b54a18b1
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-06-10 06:14:42 +0200

    add unit testing for filterByDate function

--- app/utils/filterChart.js
+++ app/utils/filterChart.js
@@ -5,8 +5,7 @@ const convertToUnixDateTime = (date) => {
 const filterChartByDate = (startDate, endDate, originalChartData) =>{
   const start = convertToUnixDateTime(startDate);
   const end = convertToUnixDateTime(endDate);
-  const clonedChartData = structuredClone(originalChartData);
-  return clonedChartData.filter((item) => (item.date_ms >= start && item.date_ms <= end));
+  return originalChartData.filter((item) => (item.date_ms >= start && item.date_ms <= end));
 };
 
 module.exports.filterChartByDate = filterChartByDate;
